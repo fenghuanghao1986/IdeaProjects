d5be21818b11bf7441dabac70135303f7e4b0359
FingerStyleFinn <hfeng7@du.edu>
2019-02-23 12:01:14 -0700

    see later changes

--- Calculator/src/com/company/OperactionConvertion.java
+++ Calculator/src/com/company/OperactionConvertion.java
@@ -4,8 +4,13 @@ import java.util.*;
 public class OperactionConvertion {
 
     public void Convert(String[] userInput) {
+
+        private ArrayList<String> newInput = new ArrayList<String>();
+
         for (int i = 0; i < userInput.length; i ++) {
-            
+            newInput.add(userInput[i]);
+
+
         }
 
     }
