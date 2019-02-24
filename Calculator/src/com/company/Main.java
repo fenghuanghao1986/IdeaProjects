3b37d783c918d717ece4ec194fa2eb41a3371620
FingerStyleFinn <hfeng7@du.edu>
2019-02-23 19:40:21 -0700

    encapsulation using set
    get value from operactionconvertion class
    and set the values to mycal object for processing
    first time learning encapsulation, very useful

--- Calculator/src/com/company/Main.java
+++ Calculator/src/com/company/Main.java
@@ -7,32 +7,38 @@ public class Main {
     public static void main(String[] args) {
         // Provide instructions for user
         System.out.println("Please provide a function you want me to calculate:");
-        System.out.println("Input format: 3 + 3 or 5 / 6 or 3 - 9 or 8 * 8");
-        System.out.println("Please make sure there is space between number and operator!");
+        System.out.println("Input format: 1+2*3*(4+5)/6");
         System.out.println("You input: ");
-
         // Getting input from user
         Scanner myObj = new Scanner(System.in);
         String userInput = myObj.nextLine();
-
         // Convert string type input to a array
         String[] realInput = userInput.split("");
 
-        // Extract all numbers to a arrlist
+        // Create operactionconvertion object
         OperactionConvertion newInput = new OperactionConvertion();
-        ArrayList<Float> test = newInput.ConvertNums(realInput);
+        // Extract all numbers
+        newInput.ConvertNums(realInput);
+        ArrayList<Float> nums = newInput.getNumsOnly();
 
-        // Extract all operators to a arrlist
-/*
-        calculator mycal = new calculator();
-        mycal.Operator(userOperation);
-        float result = mycal.calculate();
-*/
-        System.out.print(test);
+        // Extract all operators
+        newInput.ConvertOps(realInput);
+        ArrayList<String> ops = newInput.getOperatorOnly();
+
+        // Do the calculation
+        // Create calculator object
+        Calculator mycal = new Calculator();
+
+        // Set operators and numbers to mycal
+        mycal.setNums(nums);
+        mycal.setOp(ops);
+        // Calculate result and print
+        float result = mycal.Calculate();
+        System.out.print(result);
     }
 }
 
-
 // Testcase
-// 1 + 2 * 3 * 4 + 5 / 6 - 7
+// 1+2*3*4+5/6-7
 // 1+2*3*(4+5)/6
+// 12222.22*2321321-5151
