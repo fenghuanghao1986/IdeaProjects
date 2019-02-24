0ef91035442dac8911c346d21b6817801645f6cc
FingerStyleFinn <hfeng7@du.edu>
2019-02-23 19:38:52 -0700

    completing number and operator convertion
    Finishing up coding
    Fisrt complete number and operator seperating. This class can get any floating numbers and basic operators such as "+-*/()" and store them in different arraylists
    then creating encapsulation for getting those two lists for calculation
    first time learning encapsulation get and set in this project

--- Calculator/src/com/company/OperactionConvertion.java
+++ Calculator/src/com/company/OperactionConvertion.java
@@ -4,27 +4,61 @@ import java.util.*;
 public class OperactionConvertion {
 
     private String tempStr;
-    private ArrayList<Float> numsOnly = new ArrayList<Float>();
-    private ArrayList<String> operatorOnly = new ArrayList<String>();
+    private ArrayList<Float> numsOnly = new ArrayList<>();
+    private Float tempNum;
+    private ArrayList<String> operatorOnly = new ArrayList<>();
+    private ArrayList<String> tempStrArr = new ArrayList<>();
 
-    public ArrayList<Float> ConvertNums(String[] userInput) {
+    // Collect all numbers and store in one arraylist for calculation purpose
+    public void ConvertNums(String[] userInput) {
+        // Outer loop to go through all elements from userInput
+        for (int j = 0; j < userInput.length; j++) {
+            // Inner loop to collect all non-symbols and convert them to a single float number
+            for (int i = j; i < userInput.length; i++) {
+                // Once meet next operator exit inner loop
+                if (userInput[i].equals("+") || userInput[i].equals("-")
+                        || userInput[i].equals("*") || userInput[i].equals("/")
+                        || userInput[i].equals("(") || userInput[i].equals(")")) {
+                    break;
+                }
+                // Collect everything before a operator symbol and store them in a temp string array: tempStrArr
+                tempStrArr.add(userInput[i]);
+                // Convert array list to a string which only contains number look string: tempStr
+                tempStr = String.join("", tempStrArr);
+                // Convert string to a float number: tempNum
+                tempNum = Float.valueOf(tempStr.trim()).floatValue();
+                // Add 1 to j, for outer loop, in order to start the next number position properly
+                j += 1;
+            }
+            // Add number to a float list
+            numsOnly.add(tempNum);
+            // Clean the tempStrArr for next float number
+            tempStrArr = new ArrayList<>();
+        }
+    }
+
+    // Save all non-numbers including all operator symbols
+    public void ConvertOps(String[] userInput) {
 
         for (int i = 0; i < userInput.length; i++) {
-
-            ArrayList<String> tempStrArr = new ArrayList<String>();
-            tempStrArr.add(userInput[i]);
 
             if (userInput[i].equals("+") || userInput[i].equals("-")
                     || userInput[i].equals("*") || userInput[i].equals("/")
                     || userInput[i].equals("(") || userInput[i].equals(")")) {
+                operatorOnly.add(userInput[i]);
 
-                tempStr = tempStrArr.toString();
-                numsOnly.add(Float.parseFloat(tempStr));
-                i += 1;
-
+            } else {
+                continue;
             }
-
         }
+        // Add a "+" at the end for calculation purpose
+        operatorOnly.add("+");
+        //return operatorOnly;
+    }
+    public ArrayList<String> getOperatorOnly() {
+        return operatorOnly;
+    }
+    public ArrayList<Float> getNumsOnly() {
         return numsOnly;
     }
 }
