92f1090ac0ebc1a08a72a01238f0dba06302a2b6
FingerStyleFinn <hfeng7@du.edu>
2019-02-24 21:25:52 -0700

    getting inner ops and nums
    I think I have all the info for inner calculation, however, I don't know how to send these info and call calculator to do the calculation and send result to new numsArray

--- Calculator/src/com/company/singlePairParentheses.java
+++ Calculator/src/com/company/singlePairParentheses.java
@@ -5,46 +5,58 @@ public class singlePairParentheses {
 
     private ArrayList<String> innerOps = new ArrayList<>();
     private ArrayList<Float> innerNums = new ArrayList<>();
-    private ArrayList<String> operatorOnly = new ArrayList<>();
-    private ArrayList<Float> numsOnly = new ArrayList<>();
+    private ArrayList<String> orgOpsOnly = new ArrayList<>();
+    private ArrayList<Float> orgNumsOnly = new ArrayList<>();
+    private ArrayList<String> newOpsOnly = new ArrayList<>();
+    private ArrayList<Float> newNumsOnly = new ArrayList<>();
+    private ArrayList<Integer> pIndex = new ArrayList<>();
+    private int tempIndex;
+    private int cntPrentheses = 0;
     private float innerResult;
 
     public void setOp(ArrayList<String> getOperatorOnly) {
         for (int i = 0; i < getOperatorOnly.size(); i++) {
-            operatorOnly.add(getOperatorOnly.get(i));
+            orgOpsOnly.add(getOperatorOnly.get(i));
         }
     }
 
     public void setNums(ArrayList<Float> getNumsOnly) {
         for (int i = 0; i < getNumsOnly.size(); i++) {
-            numsOnly.add(getNumsOnly.get(i));
+            orgNumsOnly.add(getNumsOnly.get(i));
         }
     }
 
-    public void findOps(ArrayList<String> operatorOnly) {
-        for (int i = 0; i < operatorOnly.size(); i++) {
-            if (operatorOnly.get(i).equals("(")) {
-                for (int j = i + 1; j < operatorOnly.size(); j++) {
-                    innerOps.add(operatorOnly.get(j));
-                    if (operatorOnly.get(i).equals(")")) {
+    public void findOps(ArrayList<String> orgOpsOnly) {
+        for (int i = 0; i < orgOpsOnly.size(); i++) {
+            // creating newOpsOnly
+            newOpsOnly.add(orgOpsOnly.get(i));
+            // creating innerOps
+            if (orgOpsOnly.get(i).equals("(")) {
+                // collecting "(" index in the orgOps
+                tempIndex = i;
+                pIndex.add(i);
+                for (int j = i + 1; j < orgOpsOnly.size(); j++) {
+                    innerOps.add(orgOpsOnly.get(j));
+                    // find the right end parentheses and update operatorOnly
+                    if (orgOpsOnly.get(j).equals(")")) {
+                        // exit inner ops search and move on to next op for outter loop
+                        i = j + 1;
+                        cntPrentheses += 1;
+                        pIndex.add(i-1);
                         break;
                     }
                 }
+                break;
             }
         }
     }
 
-    public void findNums(ArrayList<Float> numsOnly) {
-        for (int i = 0; i < numsOnly.size(); i++) {
-            if (operatorOnly.get(i).equals("(")) {
-                for (int j = i; j < numsOnly.size(); j++) {
-                    innerNums.add(numsOnly.get(j));
-                    if (operatorOnly.get(i).equals(")")) {
-                        break;
-                    }
-                }
-            }
+    public void findInnerNums(ArrayList<Float> orgNumsOnly) {
+        // starting from the first index of "("
+        for (int i = pIndex.get(0); i < cntPrentheses; i++) {
+            innerNums.add(orgNumsOnly.get(i));
         }
+
     }
 
     public void calculateInnerResult() {
