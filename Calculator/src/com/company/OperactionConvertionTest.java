0178dd57390ef498b74de9d90c1748d52417cdd9
FingerStyleFinn <hfeng7@du.edu>
2019-03-10 10:04:29 -0600

    nothing much

---
+++ Calculator/src/com/company/OperactionConvertionTest.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import static org.junit.Assert.*;
+
+public class OperactionConvertionTest {
+
+    @org.junit.Test
+    public void convertNums() {
+    }
+
+    @org.junit.Test
+    public void convertOps() {
+    }
+}
